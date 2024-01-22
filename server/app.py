12aca5f70fda6e09dc4ebafbbf3ba8b4fda6e8f2
philipwanjiku <muhorophilip7@gmail.com>
2024-01-22 17:31:25 +0300

    First Flask Web Application!

--- server/app.py
+++ server/app.py
@@ -3,3 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+
+@app.route('/<string:Philip>')
+def user(Philip):
+    return f'<h1>Profile for {Philip}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555)
